5de4bbe369eea01f111c564a79d5dfaa45f3fa37
dengrunting <1032939141@qq.com>
2023-02-27 16:50:59 +0800

    fix:调整结构，方便打包

--- setup.py
+++ setup.py
@@ -2,20 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='easy_alias',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'Click',
     ],
-    # entry_points='''
-    #     [console_scripts]
-    #     paa=main:cli
-    # ''',
     entry_points={
         'console_scripts': [
-            'pp=main:cli',
-            'ppe=main:pae'
+            'pp=scripts.main:cli',
+            'ppe=scripts.main:pae'
         ],
     },
 )
